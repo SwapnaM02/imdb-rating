18af31c24ac3d9555925b9bbc892dd399529554f
SwapnaM02 <m.v.swapnareddy@gmail.com>
2024-08-01 21:51:47 +0530

    added the nav,home,components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment } from "react"
+import Home from "./components/Home.jsx"
+import Navbar from "./components/Navbar.jsx"
+import Watchlist from "./components/watchlist.jsx"
+
+
+
+
+
+
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-   <>
+   <Fragment>
+    <Navbar/>
+    <Home/>
+    <Watchlist/>
+    
   
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
   
-   </>
+   </Fragment>
   )
 }
 
