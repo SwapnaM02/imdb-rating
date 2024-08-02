71e6db5412a7903bda0eee7b3294005a3c7c1155
SwapnaM02 <m.v.swapnareddy@gmail.com>
2024-08-02 07:25:24 +0530

    css added to the file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Logo from '../assets/react.svg'
+import Logo from '../assets/imdb.png'
 import { Link } from 'react-router-dom'
+import '../App.css'
 
 const Navbar = () => {
   return (
-    <>
+    <div  className='flex space-x-8 items-center pl-3 py-4 '>
     <Link to='/'>
-      <img src={Logo} />
+      <img className="w-[50px] " src={Logo} />
     
     </Link>
     <Link to='/'> to go to home page/
@@ -14,7 +15,7 @@ const Navbar = () => {
 
     <Link to='/watchlist'> to go to watchlist page
     </Link>
-    </>
+    </div>
   )
 }
 
