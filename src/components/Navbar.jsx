539da82849204b03aaf8e9b5e1b9b717a31c3048
SwapnaM02 <m.v.swapnareddy@gmail.com>
2024-08-02 07:03:32 +0530

    raw html of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Logo from '../assets/react.svg'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      I am Navbar
-    </div>
+    <>
+    <Link to='/'>
+      <img src={Logo} />
+    
+    </Link>
+    <Link to='/'> to go to home page/
+    </Link>
+
+    <Link to='/watchlist'> to go to watchlist page
+    </Link>
+    </>
   )
 }
 
